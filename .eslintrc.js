3ea7f832202e35696c463346aea97c366a2ab4d4
bertyn99 <bboulikou@gmail.com>
2023-07-17 13:25:37 +0200

    feat add plugin eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,23 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    ecmaVersion: 2022,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'vitest',
+    'unicorn',
+    'sonarjs',
+    'prettier',
+    '@darraghor/nestjs-typed',
+  ],
   extends: [
+    'plugin:sonarjs/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:vitest/recommended',
     'plugin:prettier/recommended',
+    'plugin:@darraghor/nestjs-typed/recommended',
   ],
   root: true,
   env: {
@@ -21,5 +33,61 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'unicorn/no-fn-reference-in-iterator': 'off',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/consistent-destructuring': 'off',
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/prefer-spread': 'off',
+    'unicorn/no-array-callback-reference': 'off',
+    'unicorn/consistent-function-scoping': 'off',
+    'unicorn/no-useless-undefined': 'off',
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        allowList: { Param: true, Req: true, Res: true },
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase'],
+      },
+      {
+        selector: 'variable',
+        format: ['PascalCase', 'UPPER_CASE'],
+        types: ['boolean'],
+        prefix: ['is', 'should', 'has', 'can', 'did', 'will'],
+      },
+      {
+        selector: 'variableLike',
+        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+      },
+
+      {
+        selector: 'parameter',
+        format: ['camelCase'],
+      },
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'forbid',
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'property',
+        modifiers: ['readonly'],
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'enumMember',
+        format: ['UPPER_CASE'],
+      },
+    ],
   },
 };
