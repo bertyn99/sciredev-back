5662c058334b09c21880dfa26323a77da205c28a
Bertyn Boulikou <bboulikou@gmail.com>
2023-07-19 16:41:04 +0200

    fix some lint and format error

--- src/user/adapter/user.repository.adapter.ts
+++ src/user/adapter/user.repository.adapter.ts
@@ -1,13 +1,12 @@
-import { Injectable } from "@nestjs/common";
-import { UserRepository } from "../port/user.repository";
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from '../port/user.repository';
 
-export @Injectable()
-class UserRepositoryAdapter implements UserRepository {
-
-    createInDb() {
-        throw new Error("Method not implemented.");
-    }
-    getFromDb() {
-        throw new Error("Method not implemented.");
-    }
+@Injectable()
+export class UserRepositoryAdapter implements UserRepository {
+  createInDb() {
+    throw new Error('Method not implemented.');
+  }
+  getFromDb() {
+    throw new Error('Method not implemented.');
+  }
 }
